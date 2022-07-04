d52733931eb56121c2409afa24bf3e2de37680c2
Edmont Budakyan <edmont@abv.bg>
2022-07-04 20:17:47 +0300

    Create simpleMultiplication.c

---
+++ 00.Codewars/8 kyu/simpleMultiplication.c
@@ -0,0 +1,8 @@
+int simple_multiplication(int number) {
+    return number % 2 == 0 ? number * 8 : number * 9;
+}
+
+Alternative solution: 
+int simple_multiplication(int a) {
+    return a * (a % 2 == 0 ? 8 : 9);
+}
