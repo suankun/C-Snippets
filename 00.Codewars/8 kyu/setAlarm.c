439cc193bc83a782c9d6822b054fbe4183db4b1b
Edmont Budakyan <edmont@abv.bg>
2022-07-04 20:15:26 +0300

    Create setAlarm.c

---
+++ 00.Codewars/8 kyu/setAlarm.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool set_alarm(bool employed, bool vacation) {
+  return (employed == true && vacation == false) ? true : false;
+}
+
+Alternative solution:
+
+#include <stdbool.h>
+
+bool set_alarm(bool employed, bool vacation) {
+  return employed && !vacation;
+}
