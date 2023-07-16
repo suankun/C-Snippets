6db3ba7909bbbf24754cbfc103a36313d535b69f
suankun <edmont@abv.bg>
2023-07-16 21:44:20 +0300

    add snippets

--- Codewars/8 kyu/Simple multiplication.c
+++ Codewars/8 kyu/Simple multiplication.c
@@ -1,8 +1,8 @@
+// https://www.codewars.com/kata/583710ccaa6717322c000105/train/c
 int simple_multiplication(int number) {
     return number % 2 == 0 ? number * 8 : number * 9;
 }
-
-// Alternative solution: 
+//
 int simple_multiplication(int a) {
     return a * (a % 2 == 0 ? 8 : 9);
 }
