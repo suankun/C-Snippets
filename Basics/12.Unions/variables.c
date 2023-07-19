8588803352749ee4ae397cd271a3438258183335
suankun <edmont@abv.bg>
2023-07-19 20:19:57 +0300

    add snippets

---
+++ Basics/12.Unions/variables.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+union info {
+    char firstName[20];
+    int age;
+};
+
+// union minmax {
+//     int min;
+//     int max;
+//     double average;
+// };
+
+
+int main() {
+    union info myVariable1;
+
+    return 0;
+}
