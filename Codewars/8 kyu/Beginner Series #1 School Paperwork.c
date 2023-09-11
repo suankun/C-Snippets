b173e35cf4d5df9898266d55eb2aafcccfb795b5
suankun <edmont@abv.bg>
2023-09-11 10:05:59 +0300

    add snippets

---
+++ Codewars/8 kyu/Beginner Series #1 School Paperwork.c
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/train/c
+int paperwork(int n, int m)
+{
+    if (n<0 || m<0) return 0;
+    return n*m;
+}
+//
+int paperwork(int n, int m)
+{
+  return n > 0 && m > 0? n * m: 0;
+}
