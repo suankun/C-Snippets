eeebaf0247fe17cfce1eeed46967c5a3bde6bc31
suankun <edmont@abv.bg>
2023-09-14 19:38:38 +0300

    add snippets

---
+++ Codewars/8 kyu/get character from ASCII Value.c
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/c
+char char_of_code (unsigned charcode)
+{
+  return charcode-'0'+48;
+}
+//
+char char_of_code (unsigned charcode)
+{
+  return (char)charcode;
+}
