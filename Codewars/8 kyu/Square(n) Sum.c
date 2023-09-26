5920d13e1851b74c46850a28c0b6f30b5f92e5f3
suankun <edmont@abv.bg>
2023-09-26 13:03:48 +0300

    add snippets

---
+++ Codewars/8 kyu/Square(n) Sum.c
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/515e271a311df0350d00000f/train/c
+#include <stddef.h>
+
+int square_sum(const int values[/* count */], size_t count)
+{
+  int sum = 0;
+  for (size_t i = 0; i < count; i++) sum += (values[i] * values[i]);
+	return sum;
+}
