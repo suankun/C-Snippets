f41119ab608edbc45393f3255f7544f1dab3b232
suankun <edmont@abv.bg>
2023-09-27 21:04:31 +0300

    add snippets

---
+++ Codewars/8 kyu/Convert a Number to a String.c
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/c
+#include <stdlib.h>
+#include <stdio.h>
+
+//  return a dynamically allocated C-string
+//  this output will be freed by the tester
+
+char *number_to_string(int number) {
+    char *result = malloc(sizeof(char) * 50);
+    sprintf(result, "%d", number);
+    return result;
+}
--- Codewars/8 kyu/Return Negative.c
+++ Codewars/8 kyu/Return Negative.c
@@ -3,3 +3,8 @@ int makeNegative(int num)
 {
   return num<0 ? num : num*-1;
 }
+//
+int makeNegative(int num)
+{
+  return -abs(num);
+}
