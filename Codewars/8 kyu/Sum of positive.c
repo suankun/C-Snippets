e4c2603c800488cf887758568df014bb52998e25
suankun <edmont@abv.bg>
2023-09-28 16:20:31 +0300

    add snippets

--- Codewars/8 kyu/Sum of positive.c
+++ Codewars/8 kyu/Sum of positive.c
@@ -9,3 +9,12 @@ int positive_sum(const int *values, size_t count){
   }
   return sum;
 };
+//
+#include <stddef.h>
+
+int positive_sum(const int *val, size_t count) {
+  int sum = 0;
+  for(int i = 0; i < count; i++)
+    if(val[i] > 0) sum += val[i];
+  return sum;
+}
