78bba523351be440d532112dde5ccd98d1b80767
suankun <edmont@abv.bg>
2023-09-29 14:36:01 +0300

    add snippets

--- Codewars/8 kyu/Fake Binary.c
+++ Codewars/8 kyu/Fake Binary.c
@@ -11,3 +11,10 @@ void fakeBin(const char *digits, char *binary) {
     }
     *binary = '\0';
 }
+//
+void fakeBin(const char *digits, char *buffer) {
+  for (int i = 0; digits[i] != 0; i++) {
+    *buffer++ = digits[i] >= '5' ? '1' : '0';
+  }
+  *buffer = 0;
+}
