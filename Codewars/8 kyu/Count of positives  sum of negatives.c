251172b295be7268a6d2992878a07508c39670a1
suankun <edmont@abv.bg>
2023-10-03 11:32:38 +0300

    add snippets

---
+++ Codewars/8 kyu/Count of positives  sum of negatives.c
@@ -0,0 +1,26 @@
+//https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/c
+#include <stddef.h>
+
+void count_positives_sum_negatives(
+  int *values, size_t count, int *positivesCount, int *negativesSum) 
+{
+  for (size_t i = 0; i < count; i++) {
+    if (values[i] > 0) {
+      (*positivesCount)++;
+    } else if (values[i] < 0) {
+      *negativesSum += values[i];
+    }
+  }
+}
+//
+#include <stddef.h>
+
+void count_positives_sum_negatives(int *values, size_t count, int *positivesCount, int *negativesSum) {
+  int cnt = 0; int sm = 0;
+  for (size_t i = 0; i < count; i++) {
+    if (values[i] > 0) cnt++;
+    if (values[i] < 0) sm += values[i];
+  }
+  *positivesCount = cnt;
+  *negativesSum = sm;
+}
