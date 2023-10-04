6b081589bb8b1b602863cef3760a0787c8e589d4
suankun <edmont@abv.bg>
2023-10-04 14:06:03 +0300

    add snippets

---
+++ Codewars/8 kyu/Difference of Volumes of Cuboids.c
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/58cb43f4256836ed95000f97/train/c
+#include <stdlib.h>
+#include <math.h>
+int find_difference(const int a[3], const int b[3]) {
+  return abs((a[0]*a[1]*a[2])-(b[0]*b[1]*b[2]));
+}
