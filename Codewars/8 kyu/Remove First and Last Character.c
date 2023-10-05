c524223a51de56d380dbd79da7cbb4d50934d078
suankun <edmont@abv.bg>
2023-10-05 10:33:48 +0300

    add snippets

---
+++ Codewars/8 kyu/Remove First and Last Character.c
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/c
+#include <string.h>
+
+char* remove_char(char* dst, const char* src) {
+    size_t src_len = strlen(src);
+    if (src_len >= 2) {
+        for (size_t i = 1; i < src_len - 1; i++) {
+            dst[i - 1] = src[i];
+        }
+        dst[src_len - 2] = '\0';
+    } else {
+        dst[0] = '\0';
+    }
+
+    return dst;
+}
