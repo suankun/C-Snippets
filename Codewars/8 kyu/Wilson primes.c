5dd97516ee90466af08ca6440cdaab805bfa5878
suankun <edmont@abv.bg>
2023-10-12 23:04:52 +0300

    add snippets

--- Codewars/8 kyu/Wilson primes.c
+++ Codewars/8 kyu/Wilson primes.c
@@ -1,17 +1,21 @@
 // https://www.codewars.com/kata/55dc4520094bbaf50e0000cb/train/cpp
-bool amIWilson(unsigned n) {
+#include <stdbool.h>
+
+bool am_i_wilson(unsigned int n) {
     if (n<=1) return false;
     if (n==563) return true;
-    unsigned num {1};
-    for (unsigned i {n-1}; i>0; i--) {
+    unsigned int num = 1;
+    for (unsigned int i = n-1; i>0; i--) {
         num *= i;
     }
     
-    unsigned check = ((num)+1)/(n*n);
+    unsigned int check = ((num)+1)/(n*n);
     double result = ((num)+1.0)/(n*n);
     return check==result;
 }
 //
-bool amIWilson(unsigned n) {
+#include <stdbool.h>
+
+bool am_i_wilson(unsigned int n) {
   return n == 5 || n == 13 || n == 563;
 }
