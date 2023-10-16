c1cf8630322d9cf3c7e49d32b52341604d2261fa
suankun <edmont@abv.bg>
2023-10-16 16:57:19 +0300

    add snippets

---
+++ Codewars/7 kyu/Special Number (Special Numbers Series #5).c
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5a55f04be6be383a50000187/train/c
+const char *specialNumber(int number)
+{
+    while (number>0) {
+      int temp_num = number%10;
+      if (temp_num>5) return "NOT!!";
+      number/=10;
+    }
+    return "Special!!";
+}
