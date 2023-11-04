b07853cff77f61f9fc1598165f38eacc12d4abc5
suankun <edmont@abv.bg>
2023-11-04 19:55:39 +0200

    add snippets

--- Codewars/8 kyu/Count the number of cubes with paint on.c
+++ Codewars/8 kyu/Count the number of cubes with paint on.c
@@ -9,3 +9,9 @@ int countSquares(int cuts) {
     
     return total_cubes - cubes_with_red_face;
 }
+//
+#include <math.h>
+
+int countSquares(int cuts) {
+  return cuts ? 6 * pow(cuts, 2) + 2 : 1;
+}
