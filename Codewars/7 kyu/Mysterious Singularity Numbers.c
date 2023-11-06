1e041c69da80f28dae88051c21c73b1aea033dff
suankun <edmont@abv.bg>
2023-11-06 20:55:25 +0200

    add snippets

--- Codewars/7 kyu/Mysterious Singularity Numbers.c
+++ Codewars/7 kyu/Mysterious Singularity Numbers.c
@@ -2,3 +2,21 @@
 unsigned real_numbers(unsigned n) {
   return n - (n / 2 + n / 3 + n / 5 - n / 6 - n / 10 - n / 15 + n / 30);
 }
+//
+unsigned real_numbers(unsigned n) {
+    unsigned result=n;
+  
+    // Remove multiples of 2, 3 & 5
+    result-= n/2;
+    result-= n/3;
+    result-= n/5;
+    
+    // Add back multiples that were removed twice
+    result+= n/(2*3);
+    result+= n/(2*5);
+    result+= n/(3*5);
+  
+    // Remove again the multiples that were added back 3 times in the previous step after being removed 3 times in the first step 
+    result-= n/(2*3*5);
+    return result;
+}
