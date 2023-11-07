d4287e1030f5f223652b5380095c05cf357d7f3e
suankun <edmont@abv.bg>
2023-11-07 22:56:35 +0200

    add snippets

---
+++ Codewars/7 kyu/Sum of a sequence.c
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/586f6741c66d18c22800010a/train/c
+unsigned sequence_sum(unsigned start, unsigned end, unsigned step)
+{
+  unsigned sum = 0;
+  
+  for (unsigned num = start; num <= end; num += step) {
+    sum += num;
+  }
+  return sum;
+}
