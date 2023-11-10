3d2997bc112b11c42f2a5426ce3a3c43fcd3145b
suankun <edmont@abv.bg>
2023-11-10 22:52:23 +0200

    add snippets

---
+++ Codewars/8 kyu/Neutralisation.c
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/65128732b5aff40032a3d8f0/train/c
+#include <string.h>
+
+void neutralize(const char *s1, const char *s2, char *s3) {
+    int i = 0;
+    while (s1[i] != '\0') {
+        if (s1[i] == '+' && s2[i] == '+')
+            s3[i] = '+';
+        else if (s1[i] == '+' && s2[i] == '-')
+            s3[i] = '0';
+        else if (s1[i] == '-' && s2[i] == '+')
+            s3[i] = '0';
+        else if (s1[i] == '-' && s2[i] == '-')
+            s3[i] = '-';
+        i++;
+    }
+    s3[i] = '\0';
+}
