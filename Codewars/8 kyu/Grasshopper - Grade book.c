33a640141ae34cb35570d50011ef5e3e748712e6
suankun <edmont@abv.bg>
2023-11-16 22:19:52 +0200

    add snippets

---
+++ Codewars/8 kyu/Grasshopper - Grade book.c
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/55cbd4ba903825f7970000f5/train/c
+char get_grade(int a, int b, int c) {
+    int average=0;
+    char result;;
+    average = (a + b + c) / 3;
+    if (average >= 90 && average <= 100)
+      result = 'A';
+    else if (average >= 80 && average < 90)
+      result = 'B';
+    else if (average >= 70 && average < 80)
+      result = 'C';
+    else if (average >= 60 && average < 70)
+      result = 'D';
+    else if (average >= 0 && average < 60)
+      result = 'F';
+  
+    return result;
+}
